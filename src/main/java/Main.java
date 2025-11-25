bc17f118e443cd566e72306f948365f6c849e526
Damián <damianmn06@gmail.com>
2025-11-25 15:55:06 +0100

    Commit de la modificación 3 CORREGIDA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("\n -Esta es la modificación 1; ");
-        System.out.println("\n -Esta es la modificación 2;");
-        System.out.println("\n -Esta es la modificación 2;");
+        System.out.println("\n -Esta es la modificación 2; ");
+        System.out.println("\n -Esta es la modificación 3; ");
 
     }
 }
