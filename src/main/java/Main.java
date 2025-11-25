2df6cbc805d61c66932b3b006218572aa220aeb0
Damián <damianmn06@gmail.com>
2025-11-25 15:48:06 +0100

    Commit de la modificación 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Esta es la modificación 1; ");
+        System.out.println("\n -Esta es la modificación 1; ");
+        System.out.println("\n -Esta es la modificación 2;");
 
     }
 }
