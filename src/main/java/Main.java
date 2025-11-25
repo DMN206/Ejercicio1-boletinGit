3c6c39728c6812bdfeca39b371530c5cb30693a3
Damián <damianmn06@gmail.com>
2025-11-25 16:10:50 +0100

    Commit de la creacion de la Rama featSaludo con sus funciones hechas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,12 @@
 public class Main {
     public static void main(String[] args){
 
+        Utilidades utilidades = new Utilidades();
+
         System.out.println("\n -Esta es la modificación 1; ");
         System.out.println("\n -Esta es la modificación 2; ");
         System.out.println("\n -Esta es la modificación 3; ");
+        System.out.println(utilidades.featSaludo());
 
     }
 }
---
+++ src/main/java/Utilidades.java
@@ -0,0 +1,6 @@
+public class Utilidades {
+    public Object featSaludo(){
+        System.out.println("\nHola, esta es la función de la rama featSaludo!");
+        return 0;
+    }
+}
